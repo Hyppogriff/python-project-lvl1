359ea66a06769e6b096f34d93834600bb72a9a46
Psyker <hyppogriff@gmail.com>
2020-09-14 18:19:01 +0300

    Step 3

---
+++ Makefile
@@ -0,0 +1,5 @@
+install:
+	poetry install
+
+prompt: install
+	poetry add prompt
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,5 +1,9 @@
+import brain_games.cli
+
 def main():
-    print('Welcome to the Brain Games!')
+    print('Welcome to the Brain Games!', '\n')
+    brain_games.cli.welcome_user()
+
 
 if __name__ == '__main__':
     main()
