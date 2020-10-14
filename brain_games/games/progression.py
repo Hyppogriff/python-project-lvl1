553246289e5b54e9550975fa51172920ef1cd72a
Psyker <hyppogriff@gmail.com>
2020-10-14 23:25:57 +0300

    update progression game

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -11,22 +11,21 @@ MAX_STEP_NUMBER = 10
 
 
 def generate_question_and_answer():
-    progression = generate_progression()
+    number_first = random.randint(MIN_FIRST_NUMBER, MAX_FIRST_NUMBER)
+    number_step = random.randint(MIN_STEP_NUMBER, MAX_STEP_NUMBER)
+    progression = generate_progression(number_first, number_step)
     index = random.randint(MIN_INDEX_NUMBER, MAX_INDEX_NUMBER)
-    correct_answer = str(progression.pop(index))
-    progression.insert(index, '..')
+    correct_answer = str(progression[index])
+    progression[index] = '..'
     string = get_string_from_progression(progression)
     question = "{}".format(string)
     return correct_answer, question
 
 
-def generate_progression():
-    number_first = random.randint(MIN_FIRST_NUMBER, MAX_FIRST_NUMBER)
-    number_step = random.randint(MIN_STEP_NUMBER, MAX_STEP_NUMBER)
+def generate_progression(number_first, number_step):
     number_last = number_first + MAX_INDEX_NUMBER * number_step
-    progression = []
-    for value in range(number_first, number_last + number_step, number_step):
-        progression.append(value)
+    progression = list(range(number_first, number_last + number_step,
+                       number_step))
     return progression
 
 
